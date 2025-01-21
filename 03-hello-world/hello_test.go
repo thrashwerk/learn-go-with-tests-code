4d18559d7fa6d4c8e5bddd692dc43719c5dc93c9
Deividas Burškaitis <thrashwerk@mail25.net>
2025-01-21 17:13:40 +0200

    add more tests and functionality to Hello

--- 03-hello-world/hello_test.go
+++ 03-hello-world/hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("John", "French")
+		want := "Bonjour, John"
 
 		assertCorrectMessage(t, got, want)
 	})
