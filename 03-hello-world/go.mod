3a3b44beb210cbc869c68b958066dfcc1cc4be7b
Deividas Burškaitis <thrashwerk@mail25.net>
2025-01-21 16:49:56 +0200

    add hello world with a test

---
+++ 03-hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/thrashwerk/learn-go-with-tests-code/03-hello-world
+
+go 1.23.2
---
+++ 03-hello-world/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func main() {
+	fmt.Println(Hello("world"))
+}
+
+func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return englishHelloPrefix + name
+}
