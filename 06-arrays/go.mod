067ccd90715a6564d95955bbbf205383d7bbc691
Deividas Burškaitis <thrashwerk@mail25.net>
2025-01-29 12:51:52 +0200

    add initial code for arrays and slices chapter

---
+++ 06-arrays/go.mod
@@ -0,0 +1,3 @@
+module github.com/thrashwerk/learn-go-with-tests-code/06-arrays
+
+go 1.23.2
---
+++ 06-arrays/sum.go
@@ -0,0 +1,11 @@
+package main
+
+func Sum(numbers []int) int {
+	var sum int
+
+	for _, n := range numbers {
+		sum += n
+	}
+
+	return sum
+}
