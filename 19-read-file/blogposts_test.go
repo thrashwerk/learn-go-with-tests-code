eed0bf5a6380a10fa6675867d9ff371dd8bb162b
Deividas Burškaitis <thrashwerk@mail25.net>
2025-02-17 17:18:23 +0200

    add reading files chapter code

---
+++ 19-read-file/blogposts_test.go
@@ -0,0 +1,41 @@
+package blogposts_test
+
+import (
+	"errors"
+	"io/fs"
+	"reflect"
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/thrashwerk/learn-go-with-tests-code/19-read-file"
+)
+
+type StubFailingFS struct{}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, I always fail")
+}
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello-world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+	// _, err := blogposts.NewPostsFromFS(StubFailingFS{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got:  %d; want: %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %+v; want: %+v", got, want)
+	}
+}
---
+++ 19-read-file/go.mod
@@ -0,0 +1,3 @@
+module github.com/thrashwerk/learn-go-with-tests-code/19-read-file
+
+go 1.24.0
