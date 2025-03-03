53589ce9c96f8ae28118fca253c60fe4c6f87591
Deividas Burškaitis <thrashwerk@mail25.net>
2025-03-03 13:23:04 +0200

    add initial generics chapter code

---
+++ 21-generics/generics_test.go
@@ -0,0 +1,17 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+
+	// AssertEqual(t, 1, "1") // uncomment to see the error
+}
---
+++ 21-generics/go.mod
@@ -0,0 +1,3 @@
+module github.com/thrashwerk/learn-go-with-tests-code/21-generics
+
+go 1.24.0
