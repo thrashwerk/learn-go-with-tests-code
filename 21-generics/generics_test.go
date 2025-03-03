f665a232fb84fcc41dc37ada4e1898cbcd7146c6
Deividas Burškaitis <thrashwerk@mail25.net>
2025-03-03 14:54:14 +0200

    add stack types with tests

--- 21-generics/generics_test.go
+++ 21-generics/generics_test.go
@@ -15,3 +15,41 @@ func TestAssertFunctions(t *testing.T) {
 
 	// AssertEqual(t, 1, "1") // uncomment to see the error
 }
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(456)
+		v, _ := myStackOfInts.Pop()
+		AssertEqual(t, v, 456)
+
+		v, _ = myStackOfInts.Pop()
+		AssertEqual(t, v, 123)
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := new(StackOfStrings)
+
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("456")
+		v, _ := myStackOfStrings.Pop()
+		AssertEqual(t, v, "456")
+
+		v, _ = myStackOfStrings.Pop()
+		AssertEqual(t, v, "123")
+
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
